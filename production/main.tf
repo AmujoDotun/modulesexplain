1110efaec20ea692c833b0c02e43c00c4f35e10d
wapastic1 <joesephaws@outlook.com>
2023-02-11 14:58:05 +0100

    created s3 bucket for production envrionment

---
+++ production/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "us-west-1"
+  
+}
+
+module "production_s3_bucket" {
+  source = "../modules/s3"
+  bucket_name = "appprod-bucket"
+}
+
+module "production_s3_bucket_2" {
+  source = "../modules/s3"
+  bucket_name = "appprod-bucket-2"
+}
+
+module "production_s3_bucket_3" {
+  source = "../modules/s3"
+  bucket_name = "appprod-bucket-3"
+}
---
+++ staging/main.tf
@@ -0,0 +1,4 @@
+module "staging_s3_bucket"{
+    source = "../module"
+    bucket_name ="my-staging-bucket"
+}
